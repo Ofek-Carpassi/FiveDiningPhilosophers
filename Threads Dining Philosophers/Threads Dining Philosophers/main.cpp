1024a87484d6d9397360a4de0b4a9560f16041b0
OfekC <ofek.carpassi@gmail.com>
2025-01-27 23:00:11 +0200

    Comments and I've finished part one

--- Threads Dining Philosophers/Threads Dining Philosophers/main.cpp
+++ Threads Dining Philosophers/Threads Dining Philosophers/main.cpp
@@ -2,24 +2,28 @@
 #include <Windows.h>
 #include <ctime>
 
+// Define the amount of philosophers, forks and how many times each philosopher will eat
 #define AMOUNT_OF_PHILOSOPHERS 5
 #define AMOUNT_OF_FORKS AMOUNT_OF_PHILOSOPHERS
 #define TIMES_TO_EAT 1000000
 
+// Create a philosopher stucture so we could save for each philosopher the id and the forks he can use
 struct Philosopher {
 	int id;
 	CRITICAL_SECTION* leftFork;
 	CRITICAL_SECTION* rightFork;
 } typedef Philosopher;
 
+// The philosopher function that will be called when creating a thread
 DWORD WINAPI philosopher(LPVOID param) {
+	// Cast the parameter to a Philosopher pointer
 	Philosopher* philosopher = (Philosopher*)param;
 
 	for (int i = 0; i < TIMES_TO_EAT; i++) {
-		// Think
+		// Think (supposed to sleep but we don't want to wait in our check)
 		//Sleep(100);
 
-		// Pick up forks
+		// Pick up forks (critical section so only one philosopher can pick up a fork at a time)
 		if (philosopher->id == AMOUNT_OF_PHILOSOPHERS - 1) {
 			// Last philosopher picks up right fork first
 			EnterCriticalSection(philosopher->rightFork);
@@ -31,7 +35,7 @@ DWORD WINAPI philosopher(LPVOID param) {
 			EnterCriticalSection(philosopher->rightFork);
 		}
 
-		// Eat
+		// Eat (supposed to sleep but we don't want to wait in our check)
 		//Sleep(100);
 
 		// Put down forks
@@ -43,14 +47,16 @@ DWORD WINAPI philosopher(LPVOID param) {
 }
 
 int main() {
+	// Save a critical section for each fork and a thread for each philosopher
 	CRITICAL_SECTION forks[AMOUNT_OF_FORKS];
 	HANDLE philosophers[AMOUNT_OF_PHILOSOPHERS];
+
+	// Save the data for each philosopher
 	Philosopher philosopherData[AMOUNT_OF_PHILOSOPHERS];
 
-	// We will only use Critical Sections in this example
-	for (int i = 0; i < AMOUNT_OF_FORKS; i++) {
+	// Initialize all of the critical sections for the forks
+	for (int i = 0; i < AMOUNT_OF_FORKS; i++)
 		InitializeCriticalSection(&forks[i]);
-	}
 
 	// Create philosophers
 	for (int i = 0; i < AMOUNT_OF_PHILOSOPHERS; i++) {
@@ -64,7 +70,7 @@ int main() {
 	// Start the timer
 	clock_t start = clock();
 
-	// Create philosophers
+	// Create philosopher threads
 	for (int i = 0; i < AMOUNT_OF_PHILOSOPHERS; i++) {
 		int id = i;
 		philosophers[i] = CreateThread(
@@ -83,12 +89,12 @@ int main() {
 	// Stop the timer
 	clock_t end = clock();
 
-	// Clean up
+	// Clean up the critical sections
 	for (int i = 0; i < AMOUNT_OF_FORKS; i++) {
 		DeleteCriticalSection(&forks[i]);
 	}
 
-	// Print the time it took
+	// Print the time it took to run the program
 	std::cout << "Time: " << (double)(end - start) / CLOCKS_PER_SEC << std::endl;
 
 	return 0;
